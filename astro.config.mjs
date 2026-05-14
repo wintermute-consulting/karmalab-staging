6b764e61c17c8db5dda6eaf16788cc8d70bcfe8b
pierredemilly <pierre.de.milly@gmail.com>
2026-05-14 15:29:57 +0200

    adjusted astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wintermute-consulting.github.io/karmalab-tech',
-  base: '/',
+  site: 'https://wintermute-consulting.github.io/karmalab-staging',
+  base: '/karmalab-staging',
   integrations: [react()],
 });
